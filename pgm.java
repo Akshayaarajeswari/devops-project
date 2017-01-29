02b8f1e97646149820fc0a54967401a1607feab6
Akshayaarajeswari <git hub akpmay30@gmail.com>
2017-01-29 19:20:56 +0530

    This is my first commit

---
+++ pgm.java
@@ -0,0 +1,13 @@
+package akshu;
+
+public class pup {
+	public static void main(String[] args)
+	{
+		int n1=10;
+		int n2=20;
+		System.out.println("before swapping");
+		System.out.println("value of n1 is"+n1);
+		System.out.println(" ")
+	}
+
+}
