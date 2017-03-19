aa8bd5eb583325c180aa18fa797a557235fd62e0
Akshayaarajeswari <akpmay30@gmail.com>
2017-03-19 15:37:59 +0530

    Add files via upload
    file input program

---
+++ file.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Iotest
+{
+public static void main(string args[])
+{
+Filewriter out=new Filewriter("a.txt");
+Buffered writer b=new Buffered writer(out);
+}
+p.close();
+catch(Exception e)
+{
+}
+}
